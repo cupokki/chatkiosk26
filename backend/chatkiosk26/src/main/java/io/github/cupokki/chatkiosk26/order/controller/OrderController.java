4d1aa7c41eafbb1e4e2878bddd7c9518bd56e456
Jung <nd1346@gmail.com>
2026-08-13 22:53:02 +0900

    feat: add AI dependancies and configuration

--- backend/chatkiosk26/src/main/java/io/github/cupokki/chatkiosk26/order/controller/OrderController.java
+++ backend/chatkiosk26/src/main/java/io/github/cupokki/chatkiosk26/order/controller/OrderController.java
@@ -2,7 +2,40 @@ package io.github.cupokki.chatkiosk26.order.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/order")
+@RequiredArgsConstructor
 public class OrderController {
-    
+
+    private final OrderService orderService;
+
+    @PostMapping("/")
+    public ResponseEntity<?> checkout(@RequestBody OrderDto.CheckoutRequest req) {
+        var res = orderService.checkout(req);
+        return ResponseEntity.ok(res);
+    }
+
+    public ResponseEntity<?> getHistory() {
+        var res = orderService.getHistory();
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getDetail(@PathVariable("id") Long id) {
+        var res = orderService.getDetail(id);
+        return ResponseEntity.ok(res);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> cancel(@PathVariable("id")) {
+        var res = orderService.cancel(id);
+        return ResponseEntity.ok(res);
+    }
+
+    @PatchMapping()
+    public ResponseEntity<?> update() {
+        var res = orderService.update();
+        return ResponseEntity.ok(res);
+    }
+
+    public Repon
 }
